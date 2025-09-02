4d42fe4e5fdac38ad283e8456052e8e233a0c39e
mariana040 <marianaviana150479@gmail.com>
2025-09-02 12:27:50 -0300

    Projeto Carro

--- src/carro/ClasseAutomotores.java
+++ src/carro/ClasseAutomotores.java
@@ -1,5 +1,21 @@
 package carro;
 
 public class ClasseAutomotores {
-
+	
+	String corAt;
+	String marcaAt;
+	String modeloAt;
+	String tipoAt;
+	
+	public  ClasseAutomotores(){	
+	}
+	
+	public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar) {
+        this.corAt = corPar;
+        this.marcaAt = marcaPar;
+        this.modeloAt = modeloPar;
+        this.tipoAt = tipoPar;
+    }
+	
 }
+
